77451cf8aa95318bcbe200f0984c295b91022a2e
LFrolov <froleod@gmail.com>
2024-10-29 18:20:36 +0300

    refactoring

--- src/main/java/com/arh/prak4/demo/WebSocketConfig.java
+++ src/main/java/com/arh/prak4/demo/WebSocketConfig.java
@@ -1,30 +1,23 @@
 package com.arh.prak4.demo;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
-        config.setApplicationDestinationPrefixes("/app");
+    private final org.springframework.web.socket.WebSocketHandler webSocketHandler;
+
+    public WebSocketConfig(WebSocketHandler webSocketHandler) {
+        this.webSocketHandler = webSocketHandler;
     }
 
-//    @Override
-//    public void registerStompEndpoints(StompEndpointRegistry registry) {
-//        registry.addEndpoint("/webs").withSockJS();
-//    }
-
     @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/webs").setAllowedOriginPatterns("*").withSockJS();
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/webs").setAllowedOrigins("*");
     }
-
 }
-
--- src/main/java/com/arh/prak4/demo/WebSocketHandler.java
+++ src/main/java/com/arh/prak4/demo/WebSocketHandler.java
@@ -1,15 +1,35 @@
 package com.arh.prak4.demo;
 
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Component;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
-public class WebSocketHandler {
+public class WebSocketHandler extends TextWebSocketHandler {
 
-    @MessageMapping("/send")
-    @SendTo("/topic/messages")
-    public String processMessageFromClient(String message) {
-        return message;
+    private final Set<WebSocketSession> sessions = Collections.synchronizedSet(new HashSet<>());
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        sessions.add(session);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        for (WebSocketSession activeSession : sessions) {
+            if (activeSession.isOpen()) {
+                activeSession.sendMessage(message);
+            }
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, org.springframework.web.socket.CloseStatus status) throws Exception {
+        sessions.remove(session);
     }
 }
